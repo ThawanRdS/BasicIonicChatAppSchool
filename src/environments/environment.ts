65c36df9bc4cdeae425def1ab97bb4979e69b20a
ThawanRdS <76072320+ThawanRdS@users.noreply.github.com>
2020-12-15 18:38:47 -0300

    Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBUDeSwnrsqwpgNt833I3_CUYgzexLmi6I",
+    authDomain: "chatappetecd.firebaseapp.com",
+    databaseURL: "https://chatappetecd.firebaseio.com",
+    projectId: "chatappetecd",
+    storageBucket: "chatappetecd.appspot.com",
+    messagingSenderId: "339757121935",
+    appId: "1:339757121935:web:c96685eb6bfb4d9b908bfa",
+    measurementId: "G-MY0KVW9CZ6"
+  }
+};
